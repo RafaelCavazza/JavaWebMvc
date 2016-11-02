d5f8cb2898c831c74c926072785d4c6c576fb69e
Rafael Cavazza <rafaelcavazza@gmail.com>
2016-11-02 21:35:14 -0200

    Adicionado ao home controller a função de buscar os artistas.

--- src/java/Controller/HomeController.java
+++ src/java/Controller/HomeController.java
@@ -1,21 +1,36 @@
 package Controller;
 
+import Database.Artista;
+import Database.SqlServer;
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-        
+    
+    private SqlServer dataBase;
+    
     @RequestMapping("/Home/index.htm")
-    public ModelAndView index()
-    {
-        return new ModelAndView("/Home/index");
+    public ModelAndView index(HttpServletRequest request)
+    { 
+        ModelAndView result = new ModelAndView("/Home/index");
+        iniciaDatabase();
+        ArrayList<Artista> artistas = dataBase.BuscarArtistas();
+        result.addObject(artistas);
+        
+        return result;
     }
     
-    @RequestMapping("/Home/results.htm")
-    public ModelAndView results()
+    private void iniciaDatabase()
     {
-        return new ModelAndView("/Home/result");
+        try{
+            dataBase = new SqlServer();
+        }
+        catch(Exception ex)
+        {
+        }
     }
 }
